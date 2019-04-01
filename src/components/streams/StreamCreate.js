166114cc2aecb464606715c1a3603410e46ff502
Luis Benavides <Luisalejobg@gmail.com>
2019-04-01 09:33:51 -0300

    project finalized

--- src/components/streams/StreamCreate.js
+++ src/components/streams/StreamCreate.js
@@ -1,22 +1,21 @@
-import React, { Component } from 'react'
-import { connect } from 'react-redux'
-import { createStream } from '../../actions'
-import StreamForm  from './StreamForm'
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { createStream } from '../../actions';
+import StreamForm from './StreamForm';
 
 class StreamCreate extends Component {
-  
-  onSubmit =(formValues) => {
-    this.props.createStream(formValues)
-  }
+	onSubmit = (formValues) => {
+		this.props.createStream(formValues);
+	};
 
-  render() {
-    return (
-      <div>
-        <h3>Create a Stream</h3>
-        <StreamForm onSubmit={this.onSubmit}/>
-      </div>
-    )
-  }
+	render() {
+		return (
+			<div>
+				<h3>Create a Stream</h3>
+				<StreamForm onSubmit={this.onSubmit} />
+			</div>
+		);
+	}
 }
 
-export default connect(null,{createStream}) (StreamCreate)
+export default connect(null, { createStream })(StreamCreate);
--- src/index.js
+++ src/index.js
@@ -2,23 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware, compose } from 'redux';
-import reduxThunk from 'redux-thunk'
+import reduxThunk from 'redux-thunk';
 
-import App from './components/App'
+import App from './components/App';
 import reducers from './reducers';
 
+//! url for debug cache session  http://localhost:3000/?debug_session=labg
 
-//! url for debug cache session  http://localhost:3000/?debug_session=labg 
-
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose //! this is for redux devtools! is awesome
-const store = createStore(
-    reducers,
-    composeEnhancers(applyMiddleware(reduxThunk))
-    );
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose; //! this is for redux devtools! is awesome
+const store = createStore(reducers, composeEnhancers(applyMiddleware(reduxThunk)));
 
 ReactDOM.render(
-    <Provider store={store}>
-        <App />
-    </Provider>,
-    document.querySelector('#root')
+	<Provider store={store}>
+		<App />
+	</Provider>,
+	document.querySelector('#root')
 );
